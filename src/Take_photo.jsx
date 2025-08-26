035d91a17eb87f32d32c44ec7f40c1d2efe0606d
elroi773 <s2457@e-mirim.hs.kr>
2025-08-26 23:19:16 +0900

    choose your frame page 추가

---
+++ src/Take_photo.jsx
@@ -0,0 +1,10 @@
+import "./Take_photo.css";
+export default function Take_photo(){
+    return(
+        <div>
+            <div className="choose_container">
+                
+            </div>
+        </div>
+    )
+}
