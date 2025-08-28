99492387b46c1db8bffbd5104f2505de29e0c767
elroi773 <s2457@e-mirim.hs.kr>
2025-08-28 14:09:56 +0900

    경로 수정 / 프레임 선택 사진 수정 / 프레임 갯수에 따른 파일 이동

--- src/Take_photo.jsx
+++ src/Take_photo.jsx
@@ -3,20 +3,36 @@ import { useNavigate } from "react-router-dom";
 import twophotoImg from "./img/2photo.png";
 import fourphotoImg from "./img/4photo.png";
 import fivephotoImg from "./img/5photo.png";
-export default function Take_photo(){
-    return(
-        <div>
-            <div className="choose_container">
-                <button className="photo-btn2" onClick={() => handleNavigate("/take-photo")}>
-                    <img src={twophotoImg} id = "no2"/>
-                </button>
-                <button className="photo-btn4" onClick={() => handleNavigate("/gallery")}>
-                    <img src={fourphotoImg} id = "no3"/>
-                </button>
-                <button className="photo-btn5" onClick={() => handleNavigate("/gallery")}>
-                    <img src={fivephotoImg} id = "no4"/>
-                </button>
-            </div>
-        </div>
-    )
+
+export default function Take_photo() {
+  const navigate = useNavigate();
+
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <div>
+      <div className="choose_container">
+        <button
+          className="photo-btn2"
+          onClick={() => handleNavigate("/twophoto")}
+        >
+          <img src={twophotoImg} id="no2" />
+        </button>
+        <button
+          className="photo-btn4"
+          onClick={() => handleNavigate("/fourphoto")}
+        >
+          <img src={fourphotoImg} id="no4" />
+        </button>
+        <button
+          className="photo-btn5"
+          onClick={() => handleNavigate("/fivephoto")}
+        >
+          <img src={fivephotoImg} id="no5" />
+        </button>
+      </div>
+    </div>
+  );
 }
