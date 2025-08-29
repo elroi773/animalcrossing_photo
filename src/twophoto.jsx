a2c725ca91a7c5a2bd5dced29069eec9774e23ae
elroi773 <s2457@e-mirim.hs.kr>
2025-08-29 21:16:09 +0900

    주민 불러오기

--- src/twophoto.jsx
+++ src/twophoto.jsx
@@ -1,3 +1,53 @@
-export default function twophoto(){
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Webcam from "react-webcam";
+import styles from "./photo.module.css";
+import startbtn from "./img/start_button.png";
 
+function Twophoto() {
+  const [villager, setVillager] = useState(null);
+
+  // 랜덤 주민 데이터 가져오기
+  const fetchVillager = async () => {
+    try {
+      const response = await axios.get("https://api.nookipedia.com/villagers", {
+        headers: {
+          "X-API-KEY": "e25f17a9-12b3-4033-8f59-da299adfe32a",
+          "Accept-Version": "1.0.0",
+        },
+      });
+      const data = response.data;
+      const randomVillager = data[Math.floor(Math.random() * data.length)];
+      setVillager(randomVillager);
+    } catch (err) {
+      console.error("주민 데이터를 불러오지 못했습니다:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchVillager();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Webcam
+        className={styles.webcam}
+        audio={false}
+        videoConstraints={{ facingMode: "user" }}
+      />
+
+      {villager && (
+        <div className={styles.villagerOverlay}>
+          <img src={villager.image_url} alt={villager.name} />
+          <p>{villager.name}</p>
+        </div>
+      )}
+
+      <button className={styles.button}>
+        <img src={startbtn} alt="Start" />
+      </button>
+    </div>
+  );
 }
+
+export default Twophoto;
