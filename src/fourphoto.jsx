5cbddd46c94bd7fa2f32cc0a82b326c3de04a764
elroi773 <s2457@e-mirim.hs.kr>
2025-08-29 21:34:48 +0900

    다섯개 사진 네개 사진 버전 추가

--- src/fourphoto.jsx
+++ src/fourphoto.jsx
@@ -1,3 +1,56 @@
-export default function fourphoto(){
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Webcam from "react-webcam";
+import styles from "./photo.module.css";
+import startbtn from "./img/start_button.png";
 
+function Fourphoto() {
+  const [villager, setVillager] = useState(null);
+
+  const fetchVillager = async () => {
+    try {
+      const response = await axios.get("https://api.nookipedia.com/villagers", {
+        headers: {
+          "X-API-KEY": "e25f17a9-12b3-4033-8f59-da299adfe32a",
+          "Accept-Version": "1.0.0",
+        },
+      });
+      const data = response.data;
+      const randomVillager = data[Math.floor(Math.random() * data.length)];
+      setVillager(randomVillager);
+    } catch (err) {
+      console.error("주민 데이터를 불러오지 못했습니다:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchVillager();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.webcamArea}>
+        <div className={styles.webcamWrapper}>
+          <Webcam
+            className={styles.webcam}
+            audio={false}
+            videoConstraints={{ facingMode: "user" }}
+          />
+  
+          {villager && (
+            <div className={styles.villagerOverlay}>
+              <img src={villager.image_url} alt={villager.name} />
+              <p>{villager.name}</p>
+            </div>
+          )}
+        </div>
+  
+        <button className={styles.button}>
+          <img src={startbtn} alt="Start" />
+        </button>
+      </div>
+    </div>
+  );  
 }
+
+export default Fourphoto;
